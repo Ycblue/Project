d9eade4766bbbd8291960e435e40c8ca3aad2404
Ycblue <yuchialan@gmail.com>
2016-04-11 18:01:48 +0800

    Added files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	$("#p1").on({
+		"mouseover": function(){
+			$("#pic").attr('src',"Pictures/x1.jpeg");
+		}
+	})
+	$("#p2").on({
+		"mouseover": function(){
+			$("#pic").attr('src',"Pictures/x2.jpeg");
+		}
+	})
+	$("#p3").on({
+		"mouseover": function(){
+			$("#pic").attr('src',"Pictures/x3.jpeg");
+		}
+	})
+	$("#p4").on({
+		"mouseover": function(){
+			$("#pic").attr('src',"Pictures/x4.jpeg");
+		}
+	})
+	$("#p5").on({
+		"mouseover": function(){
+			$("#pic").attr('src',"Pictures/x5.jpeg");
+		}
+	})
+	$("#p6").on({
+		"mouseover": function(){
+			$("#pic").attr('src',"Pictures/x6.jpeg");
+		}
+	})
+	$("#p7").on({
+		"mouseover": function(){
+			$("#pic").attr('src',"Pictures/x7.jpeg");
+		}
+	})
+});
